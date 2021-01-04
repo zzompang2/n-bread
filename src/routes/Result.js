4bd07b53f95c02cd1b98ad851d392c99795e8542
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2021-01-05 04:25:48 +0900

    calculate subsets with zero sum

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -2,10 +2,65 @@ import React from 'react';
 import "./Result.css";
 
 class Result extends React.Component {
+
+	getMaximumSubsets(paidMoney) {
+		const money = [...paidMoney];
+		const subsets = [[]];			/* 모든 subset 의 index array */
+		const result = [];				/* 합이 0 이 되는 subset 의 index array */
+
+		/* 모든 subset 의 index array 만들기.
+		 * 현재까지의 부분집합 + 현재까지의 부분집합에 i 번째 원소를 넣은 부분집합 */
+		for (let i=0; i<money.length; i++) {
+			const len = subsets.length;
+			for (let j=0; j<len; j++) {
+				const subset = [...subsets[j], i];	/* new subset */
+				let sum = 0;
+
+				/* 길이가 전체의 절반 이상인데 합이 0 이려면,
+				   이미 구한 RESULT 의 여집합인 경우 뿐이다 */
+				if (subset.length > money.length/2)
+					continue;
+
+				/* SUBSET 의 총 금액 SUM 을 구한다
+				   금액이 undefined 인 index 는 이미 RESULT 에 들어간 것이므로 제외 */
+				for (let k=0; k<subset.length;) {
+					if (money[subset[k]] !== undefined) {
+						sum += money[subset[k]];
+						k += 1;
+					}
+					else
+						subset.splice(k, 1);
+				}
+				/* SUM 이 0 인 SUBSET 인 경우 RESULT 에 추가하고
+					 다른 조합에 원소들이 포함되지 않도록 MONEY 값을 undefined 으로 한다.
+					 그리고 I 번째 원소를 포함한 조합은 더이상 있을 수 없으므로 break. */
+				if (sum === 0 && subset.length !== 0) {
+					result.push(subset);
+					for (let k=0; k<subset.length; k++)
+						money[subset[k]] = undefined;
+					break;
+				}
+				else
+					subsets.push(subset);
+			}
+		}
+		/* 남은 원소들이 있는 경우 (합은 0 이지만 길이가 전체 길이의 절반 초과인 경우) */
+		const rest = [];
+		for (let i=0; i<money.length; i++)
+			if (money[i] != undefined)
+				rest.push(i);
+		if (rest !== [])
+			result.push(rest);
+
+		return result;
+	}
+	
 	render() {
-		const { location: { state }, history } = this.props;
+		const { location: { state } } = this.props;
 		const { number, people, payments } = state;
-		const totalPaidMoney = [];
+		const { getMaximumSubsets } = this;
+
+		const totalPaidMoney = [];		/* 각자 지불해야 할 총 금액 */
 
 		for (let i=0; i<number; i++)
 			totalPaidMoney.push(0);
@@ -13,28 +68,29 @@ class Result extends React.Component {
 		for (let i=0; i<payments.length; i++) {
 			const { payer, joins } = payments[i];
 			let money = Number(payments[i].money);
-			let joinNum = 0;
+			let joinNum = 0;	/* 해당 지불 항목에 대한 총 참가자 수 */
 
+			/* 총 참가자 수를 구한다 */
 			for (let j=0; j<joins.length; j++)
 				if (joins[j])
 					joinNum += 1;
 
-			// 나누어 떨어지지 않으면 payer 에게 올림해서 보내기로 한다
+			/* 나누어 떨어지지 않으면 올림해서 보내기로 한다 */
 			const rest = money % joinNum;
 			if (rest !== 0)
 				money += (joinNum - rest);
 
-			// payer 는 사용한 금액만큼 제외
+			/* PAYER 는 사용한 금액만큼 제외 */
 			totalPaidMoney[payer] -= money;
 
-			// 참가자들끼리 나누어 지불한다
-			for (let j=0; j<joins.length; j++) {
+			/* n 등분하여 각 참가자들의 지불 금액에 추가한다 */
+			for (let j=0; j<joins.length; j++)
 				if (joins[j])
 					totalPaidMoney[j] += money / joinNum;
-			}
 		}
 
-		console.log(totalPaidMoney);
+		const subsets = getMaximumSubsets(totalPaidMoney);
+		console.log(subsets);
 
 		return (
 			<div>
